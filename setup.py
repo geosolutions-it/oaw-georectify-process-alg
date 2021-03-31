bbebd5e1fae2783bd82fd7f31dc55a657168dddc
mattiagiupponi <51856725+mattiagiupponi@users.noreply.github.com>
2021-03-31 14:12:41 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,6 @@
 from setuptools import find_packages, setup
 setup(
     name='geotiflib',
-    packages=find_packages(include=['geotiflib']),
     version='0.1.1',
     description='GeoTiff Python library utils',
     author='GeoSolutions',
