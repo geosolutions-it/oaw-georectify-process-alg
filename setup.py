fa045fac62db764a9712e204ec8364f82e9d4408
Christian R. Picone <christian.picone@geo-solutions.it>
2021-03-31 16:44:42 +0200

    add support for the ouput_folder parameter

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 setup(
     name='geotiflib',
     packages=find_packages(include=['geotiflib']),
-    version='0.1.0',
+    version='1.0.0',
     description='GeoTiff Python library utils',
     author='GeoSolutions',
     license='GNU General Public License v3',
