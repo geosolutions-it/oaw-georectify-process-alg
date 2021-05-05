cefa2a407faece308ad266f953d77c90692f113f
Christian R. Picone <christian.picone@geo-solutions.it>
2021-05-05 15:39:21 +0200

    version 1.0.11

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import find_packages, setup
 setup(
     name='geotiflib',
-    version='1.0.10',
+    version='1.0.11',
     description='GeoTiff Python library utils',
     author='GeoSolutions',
     license='GNU General Public License v3',
